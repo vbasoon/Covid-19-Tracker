de71e698afe9be208698146d5ead11c5593b11f7
Басун Володимир <vbasoon@gmail.com>
2021-03-02 08:35:49 +0200

    country

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
+import {
+  FormControl,
+  Select,
+  MenuItem
+} from '@material-ui/core'
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState(['USA','UK','Ukraine']);
   return (
     <div className="App">
+      <div className="app__header">
       <h1>Covid Tracker</h1>
+      <FormControl className="app_dropdown">
+        <Select 
+          variant="outlined"
+          value="abc"
+        >
+
+          {
+            countries.map((country) =>(
+              <MenuItem value={country}>{country}</MenuItem>
+            ))
+          }
+          {/* 
+          <MenuItem value="worldWide">Option 2</MenuItem>
+          <MenuItem value="worldWide">Option 3</MenuItem>
+          <MenuItem value="worldWide">Option 4</MenuItem>   */}
+        </Select>
+      </FormControl>
+
+
+
       {/*
       Header
       Title + Select input dropdown field
@@ -17,7 +44,7 @@ function App() {
       Graph
       Map 
       */}
-
+</div>
     </div>
   );
 }
